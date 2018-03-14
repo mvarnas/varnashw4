3becaf9f49ed288344e66741228772479f0d54a0
mvarnas <michelle.varnas@gmail.com>
2018-03-13 19:34:58 -0700

    updated fetch

--- website/fetch.js
+++ website/fetch.js
@@ -1,52 +1,17 @@
-
-// function yousuck(){
-	fetch('https://jsonplaceholder.typicode.com/users',{method: 'get'
-}).then(function(response){
-	const r = response.json();
-	return r;
-}).then(function(s){
-	document.getElementById('fetchpromise').innerHTML = s;
-})
-// }
-
-// yousuck();
-
-
-// }).catch(function(err) { 
-// 	// Error, handle err 
-// });
-
-// Promise.all([ 
-// 	asyncFunc1({username}), 
-// 	asyncFunc2(), 
-// ]) 
-// .then(([result1, result2]) => { 
-// }) 
-
-// function loadJson(url) {
-// 	return fetch(url)
-// 	.then(response => response.json());
-//   }
-  
-//   function loadJsonUserName(username) {
-// 	return fetch(`https://jsonplaceholder.typicode.com/users${username}`)
-// 	  .then(response => response.json());
-//   }
-  
-//   function showAvatar(githubUser) {
-// 	return new Promise(function(resolve, reject) {
-// 	  let img = document.createElement('img');
-// 	  img.src = githubUser.avatar_url;
-// 	  img.className = "promise-avatar-example";
-// 	  document.body.append(img);
-  
-// 	  setTimeout(() => {
-// 		img.remove();
-// 		resolve(githubUser);
-// 	  }, 3000);
-// 	});
-//   }
-//   loadJson('dorthe/varnashw4/fetch.js')
-//   .then(username => loadJsonUserName(username))
-//   .then(showAvatar)
-//   .then(githubUser => alert(`Finished showing ${githubUser.username}`));
+fetch("https://jsonplaceholder.typicode.com/users")
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(s) {
+        fetchSort(s);
+    });
+function fetchSort(p){
+    let userNameArray = [];
+    for (let i=0; i<p.length; i++){
+        userNameArray.push(p[i].username)
+    }
+    userNameArray.sort(function(short, long){
+    return short.length - long.length;
+    });
+    document.getElementById('fetchpromise').innerHTML = userNameArray;
+}
