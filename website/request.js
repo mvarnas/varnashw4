57af69beca722b38514b693fd3da98a523d089c3
mvarnas <michelle.varnas@gmail.com>
2018-03-13 18:25:18 -0700

    array sorted

--- website/request.js
+++ website/request.js
@@ -1,13 +1,22 @@
-const req = new XMLHttpRequest(); 
-req.open('GET', 'https://jsonplaceholder.typicode.com/users'); 
-req.onload = function () { 
-	if (req.status == 200) { 
-		document.getElementById('callback').innerHTML = req.response; 
-	} else { 
-		console.log('ERROR', req.statusText); 
+const req = new XMLHttpRequest();
+req.open('GET', 'https://jsonplaceholder.typicode.com/users');
+req.onload = function () {
+	if (req.status == 200) {
+		let p = JSON.parse(req.response);
+		let emailArray = [];
+		let o = '';
+		for (let i = 0; i < p.length; i++) {
+			o = p[i].email;
+			emailArray.push(o);
+		}
+		s=emailArray.sort()
+		document.getElementById('callback').innerHTML = s;
+		
+	} else {
+		console.log('ERROR', req.statusText);
 	}
 };
-req.onerror = function () { 
-	console.log('Network Error'); 
-}; 
-req.send(); 
+req.onerror = function () {
+	console.log('Network Error');
+};
+req.send();
